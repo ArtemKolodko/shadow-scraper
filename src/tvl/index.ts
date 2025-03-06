421fa93875ecf47cf6b1b092ecc983f88b7c0b65
artemkolodko <temakolodko@gmail.com>
2025-03-06 11:06:17 +0000

    Added USD value

--- src/tvl/index.ts
+++ src/tvl/index.ts
@@ -1,11 +1,14 @@
 import {bootstrapDb} from "../db";
 import {MappedClBurn, MappedClMint} from "../types";
+import {getTokensPrice, CoinGeckoTokensMap} from "../api/coingecko";
 
 const main = async () => {
   const db = bootstrapDb()
 
   let token0Amount = 0
+  let token0AmountUsd = 0
   let token1Amount = 0
+  let token1AmountUsd = 0
   let token0Symbol = ''
   let token1Symbol = ''
 
@@ -29,7 +32,30 @@ const main = async () => {
     token1Amount -= Number(burn.amount1)
   }
 
-  console.log(`${token0Symbol} amount=${token0Amount}, ${token1Symbol} amount=${token1Amount}`)
+  const token0Coingecko = CoinGeckoTokensMap[token0Symbol]
+  const token1Coingecko = CoinGeckoTokensMap[token1Symbol]
+  if(token0Coingecko && token1Coingecko) {
+    const prices = await getTokensPrice([
+      token0Coingecko,
+      token1Coingecko
+    ])
+    token0AmountUsd = prices[token0Coingecko].usd * token0Amount
+    token1AmountUsd = prices[token1Coingecko].usd * token1Amount
+  }
+
+  console.log(`${
+    token0Symbol
+  } amount=${
+    token0Amount
+  } ($${
+    token0AmountUsd
+  }), ${
+    token1Symbol
+  } amount=${
+    token1Amount
+  } ($${
+    token1AmountUsd
+  })`)
 }
 
 main()
